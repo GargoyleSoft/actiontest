39dd7e78ab8de8d16dbc4299199a4857cb2b1eca
Scott Grosch <Scott.Grosch@icloud.com>
2022-10-09 16:54:24 -0700

    x

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import {context, getOctokit} from '@actions/github'
 
 const {repo: { owner, repo }} = context
 
-// const tester = getInput('tester')
+const tester = getInput('tester')
 const githubToken = getInput('github-token')
 const octokit = getOctokit(githubToken)
 console.info(JSON.stringify(context))
