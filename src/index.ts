e6954c2ab157acffac331f5fc847258d3e2a33a1
Scott Grosch <scott.grosch@intel.com>
2022-10-09 16:32:26 -0700

    x

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { getInput } from '@actions/core'
+import {context, getOctokit} from '@actions/github'
+
+const {repo: { owner, repo }} = context
+
+const tester = getInput('tester')
+const githubToken = getInput('github-token')
+const octokit = getOctokit(githubToken)
+console.info(JSON.stringify(context))
+
